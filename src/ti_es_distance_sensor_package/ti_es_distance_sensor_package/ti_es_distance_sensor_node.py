d8bbd8c9cf8615fbce91b756246cf4dffc77ac58
¨Tecert¨ <1027668@hr.nl>
2025-04-15 01:25:08 +0200

    Added publisher_ and log_publisher_

--- src/ti_es_distance_sensor_package/ti_es_distance_sensor_package/ti_es_distance_sensor_node.py
+++ src/ti_es_distance_sensor_package/ti_es_distance_sensor_package/ti_es_distance_sensor_node.py
@@ -3,87 +3,107 @@
 # Sometimes I get this out of nowhere No distance data received from I2C device with address 8 and bus 1, probably because the rasperry is requesting too much too soon
 # Ask Nico how to implement log code in here
 
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
 import lgpio
 import time
-# Variable used for I2C_open and I2C_close
-I2C_connection = None
-
-# I2C address
-I2C_addr = 0x08 # bus address
-
-# I2C bus 1 is chosen for communication with I2C device
-I2C_bus = 1
-
-# read bytes
-I2C_read_bytes = 1
 
 
-# Bit 0 is a verification bit to verify if data has been sent to the master when requested
-# If data has been sent to the master from the slave the verification bit(bit 0) will be 1
-# Otherwise the bit will be zero
+class UltrasoneSensorNode(Node):
+    def __init__(self):
+        super().__init__('ultrasone_sensor_node')
 
-# For now four sensors have been connected to the Arduino Uno. This is how the communiction works:
+        # I2C Config
+        self.I2C_addr = 0x08  # I2C address of Arduino
+        self.I2C_bus = 1
+        self.I2C_read_bytes = 1
+        self.I2C_connection = None
 
-# 0x00 = no data has been sent from the slave to the master, this data is incorrect 
-# 0x01 = data has been sent from the slave to the master, this data is correct
+        # Timer for polling the sensor
+        self.timer_period = 0.25  # seconds
+        self.timer = self.create_timer(self.timer_period, self.read_sensor_data)
 
-# Bits designated for the sensors work as follows
-# 0 means that there is no detection within 10 cm and 1 means an object has been detected within 10 cm of the sensor
+        # Publisher using String messages
+        self.publisher_ = self.create_publisher(String, 'distance', 10)
+        self.log_publisher_ = self.create_publisher(String, "ti/es/logger_data",10)
 
-# Bit 1 is for <name of ultrasone sensor one>
-# Bit 2 is for <name of ultrasone sensor two>
-# Bit 3 is for <name of ultrasone sensor three>
-# Bit 4 is for <name of ultrasone sensor four>
+        # Connect to I2C device
+        self.connect_I2C()
 
-# To see where the placements of the ultrasone sensors go to this document : <document of the position of the ultrasone sensors on the telescope>
-def connect_I2C():
-    global I2C_connection
-    # Try to open bus 1 with slave address 8. if bus can't be opened an error message is printed on the screen
+    def publish_log(self, level: str, message: str):
+        log_msg = String()
+        timestamp = time.strftime('%H:%M')
+        log_msg.data = f"[{timestamp}][{level}][distance_sensor] {message}"
+        self.log_publisher_.publish(log_msg)
+        # Bit 0 is a verification bit to verify if data has been sent to the master when requested
+        # If data has been sent to the master from the slave the verification bit(bit 0) will be 1
+        # Otherwise the bit will be zero
+
+        # For now four sensors have been connected to the Arduino Uno. This is how the communiction works:
+
+        # 0x00 = no data has been sent from the slave to the master, this data is incorrect 
+        # 0x01 = data has been sent from the slave to the master, this data is correct
+
+        # Bits designated for the sensors work as follows
+        # 0 means that there is no detection within 10 cm and 1 means an object has been detected within 10 cm of the sensor
+
+        # Bit 1 is for <name of ultrasone sensor one>
+        # Bit 2 is for <name of ultrasone sensor two>
+        # Bit 3 is for <name of ultrasone sensor three>
+        # Bit 4 is for <name of ultrasone sensor four>
+
+        # To see where the placements of the ultrasone sensors go to this document : <document of the position of the ultrasone sensors on the telescope>
+    def connect_I2C(self):
+        try:
+            self.I2C_connection = lgpio.i2c_open(self.I2C_bus, self.I2C_addr)
+            self.publish_log("info", f"I2C connection opened on bus {self.I2C_bus} with address {self.I2C_addr}")
+        except Exception:
+            self.publish_log("error", f"Could not open I2C connection on bus {self.I2C_bus} with address {self.I2C_addr}")
+
+    def read_sensor_data(self):
+        if self.I2C_connection is None:
+            self.publish_log("warning", f"No I2C connection established on bus {self.I2C_bus} with address {self.I2C_addr}")
+            return
+
+        try:
+            data = lgpio.i2c_read_device(self.I2C_connection, self.I2C_read_bytes)
+            sensor_byte = data[1][0]
+
+            if sensor_byte == 0:
+                self.publish_log("warning", f"No distance data received from slave device on bus {self.I2C_bus} with address {self.I2C_addr}")
+            else:
+                binary_repr = bin(sensor_byte)[2:].zfill(8)
+                self.get_logger().info(f"Sensor Byte (bin): {binary_repr}")
+
+                msg = String()
+                msg.data = f"Sensor byte: {binary_repr}"
+                self.publisher_.publish(msg)
+
+        except Exception:
+            self.publish_log("error", f"Error reading I2C slave device on bus {self.I2C_bus} with address {self.I2C_addr}")
+
+    def destroy_node(self):
+        if self.I2C_connection is not None:
+            lgpio.i2c_close(self.I2C_connection)
+            self.publish_log("info", f"Closed I2C connection on bus {self.I2C_bus} with address {self.I2C_addr}")
+        super().destroy_node()
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = UltrasoneSensorNode()
+
     try:
-       I2C_connection = lgpio.i2c_open(I2C_bus, I2C_addr)
+        rclpy.spin(node)
     except KeyboardInterrupt:
-        raise KeyboardInterrupt
-    except:
-        print(f"Could not open bus with number {I2C_bus} and connect to I2C device at address {I2C_addr}")
-        return
-    
-    try:
-        data = lgpio.i2c_read_device(I2C_bus,I2C_read_bytes)
-    except KeyboardInterrupt:
-        raise KeyboardInterrupt
-    except:
-        print(f"Connection could not be established with I2C device at address {I2C_addr} and bus2 {I2C_bus}")
-        return
-    
+        print("Keyboardinterrupt")
+        # node.get_logger().info("KeyboardInterrupt: shutting down node...")
+    finally:
+        node.destroy_node()
 
-    try:
-        # If no data is received and the first value of the array is one, raise a I2CNoDataError
-        if(data[1][0] == 0):
-            print(f"No distance data received from I2C device with address {I2C_addr} and bus {I2C_bus}")
-        else:
-            print(data[1][0])
-
-    except KeyboardInterrupt:
-        raise KeyboardInterrupt
-
-
-def main():
-    interval = 0.25
-    next_time = time.time() + interval
-    try:
-        while True:
-            current_time = time.time()
-            if current_time >= next_time:
-                connect_I2C()
-                next_time += interval
-            time.sleep(0.001)
-            
-
-    # Handle Keyboard Exception
-    except KeyboardInterrupt:
-        print("KeyboardInterrupt")
-        if I2C_connection!= None:
-            lgpio.i2c_close(I2C_connection)
-
+        
 if __name__ == '__main__':
     main()
